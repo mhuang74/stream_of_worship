d252ab405d95a933259e471e35bc3edf589ddcba
Michael Huang <mhuang74@gmail.com>
2026-06-04 18:46:52 +0800

    fix: split migration 0012 into separate statements for Neon HTTP driver

--- webapp/drizzle/0012_modern_exiles.sql
+++ webapp/drizzle/0012_modern_exiles.sql
@@ -1,4 +1,4 @@
-ALTER TABLE "songsets" ADD COLUMN "last_completed_render_job_id" text;
+ALTER TABLE "songsets" ADD COLUMN "last_completed_render_job_id" text;--> statement-breakpoint
 
 UPDATE "songsets" SET "last_completed_render_job_id" = "latest_render_job_id"
   WHERE "latest_render_job_id" IN (SELECT "id" FROM "render_jobs" WHERE "status" = 'completed');
